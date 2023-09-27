8793a7e6ab75acf03e82c76324e2bba12a49a1ec
RYuriy <radov.yuriy@ukr.net>
2023-09-27 12:13:54 +0300

    + first Unit test

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import com.um191.Main;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class MainTest {
+   @Test
+    public void testGetGreetings() {
+       String expected = "Hello and welcome Sasha!";
+       String actual = Main.getGreetings("Sasha");
+       Assert.assertEquals(expected, actual);
+    }
+}
